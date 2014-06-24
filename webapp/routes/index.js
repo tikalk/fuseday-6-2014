5871eca0622ab8f9c02ed83e053a3125f06b0ced
John Smith <assaf@youappi.com>
2014-06-24 14:32:40 +0300

    added redis DAO

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,4 +1,18 @@
 
+'use strict';
+
+var DataService = require('../lib/DataService'),
+    argv = require('optimist')
+        .usage('Usage: $0 -redisPort [num] -redisHost [num]')
+        .demand(['redisPort','redisHost']).argv;
+
+var redisConf = {
+    port: argv.redisPort,
+    host: argv.redisHost
+};
+
+var dataService = new DataService(redisConf);
+
 exports.home = function(req, res, next){
     res.send('Hello Fuseday');
 
